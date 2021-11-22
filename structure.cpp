7def479c1e0d98607be16f96a0d1d3a80be61922
Keyur Maru <kd2maru@gmail.com>
2021-11-22 01:13:07 -0800

    basics: double-nested structure

--- structure.cpp
+++ structure.cpp
@@ -1,12 +1,16 @@
-#include<iostream>
+ #include<iostream>
 #include<string>
 using namespace std;
 
+struct rearCamera { //double-nested
+    int main;
+    int wide;
+    int tele;
+};
+
 struct camera {
-    int front;
-    int rearMain;
-    int rearWide;
-    int rearTele;
+    int frontCamera;
+    rearCamera rearCamera;
 };
 
 struct phone {
@@ -20,31 +24,31 @@ int main() {
     phone samsung;
     samsung.brand = "Samsung";
     samsung.model = "S22 Ultra";
-    samsung.camera.front = 16;
-    samsung.camera.rearMain = 102;
-    samsung.camera.rearTele = 16;
-    samsung.camera.rearWide = 12;
+    samsung.camera.frontCamera = 16;
+    samsung.camera.rearCamera.main = 102;
+    samsung.camera.rearCamera.tele = 16;
+    samsung.camera.rearCamera.wide = 12;
 
     phone apple;
     apple.brand = "Apple";
     apple.model = "iPhone 14 Pro Max";
-    apple.camera.front = 12;
-    apple.camera.rearMain = 16;
-    apple.camera.rearTele = 12;
-    apple.camera.rearWide = 12;
+    apple.camera.frontCamera = 12;
+    apple.camera.rearCamera.main = 16;
+    apple.camera.rearCamera.tele = 12;
+    apple.camera.rearCamera.wide = 12;
 
     cout << "Brand: " << samsung.brand << endl;
     cout << "Model: " << samsung.model << endl;
-    cout << "Front Camera: " << samsung.camera.front << endl;
-    cout << "Rear Main Camera: " << samsung.camera.rearMain << endl;
-    cout << "Rear Telephoto Camera: " << samsung.camera.rearTele << endl;
-    cout << "Rear Wideangle Camera: " << samsung.camera.rearWide << endl;
+    cout << "Front Camera: " << samsung.camera.frontCamera << endl;
+    cout << "Rear Main Camera: " << samsung.camera.rearCamera.main << endl;
+    cout << "Rear Telephoto Camera: " << samsung.camera.rearCamera.tele << endl;
+    cout << "Rear Wideangle Camera: " << samsung.camera.rearCamera.wide << endl;
 
     cout << endl << "Brand: " << apple.brand << endl;
     cout << "Model: " << apple.model << endl;
-    cout << "Front Camera: " << apple.camera.front << endl;
-    cout << "Rear Main Camera: " << apple.camera.rearMain << endl;
-    cout << "Rear Telephoto Camera: " << apple.camera.rearTele << endl;
-    cout << "Rear Wideangle Camera: " << apple.camera.rearWide << endl;
+    cout << "Front Camera: " << apple.camera.frontCamera << endl;
+    cout << "Rear Main Camera: " << apple.camera.rearCamera.main << endl;
+    cout << "Rear Telephoto Camera: " << apple.camera.rearCamera.tele << endl;
+    cout << "Rear Wideangle Camera: " << apple.camera.rearCamera.wide << endl;
     return 0;
 }
