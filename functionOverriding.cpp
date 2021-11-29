1cb7d77629d5cdfc95fab6cbb896528c4bdcd85f
Keyur Maru <kd2maru@gmail.com>
2021-11-29 00:19:58 -0800

    oops: function overriding

---
+++ functionOverriding.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+class Animal {
+public:
+    void makeSound() {
+        cout << "This is default sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void makeSound() {
+        cout << "Woof Woof!" << endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    void makeSound() {
+        cout << "Meow Meow" << endl;
+        Animal::makeSound();                    //calling default makeSound from base class Animal
+    }
+};
+
+int main() {
+
+    Animal a;
+    Dog d;
+    Cat c;
+
+    a.makeSound();
+    d.makeSound();
+    c.makeSound();
+
+
+}
